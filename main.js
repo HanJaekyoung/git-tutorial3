c1d46216a9778227bdba2a9cd0f3d2ebcc95708c
HanJaekyoung <102638110+HanJaekyoung@users.noreply.github.com>
2023-02-02 11:56:27 +0900

    commit

---
+++ main.js
@@ -0,0 +1,36 @@
+class Counter{
+    constructor(runEveryFiveTimes){
+        this.counter = 0;
+        this.callback = runEveryFiveTimes;
+    }
+
+    increase(){
+    this.counter++;
+    console.log(this.counter);
+    if  (this.counter % 5 === 0){
+        this.callback && this.callback(this.counter);
+        
+       
+        }
+    }
+}
+
+function printSomething(num){
+    console.log(`excellent! ${num}`);
+}
+function alertNum(num){
+    alert(`excellent! ${num}`);
+}
+
+
+const printCounter = new Counter(printSomething);
+const alertCounter = new Counter(alertNum);
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase(alertNum);
